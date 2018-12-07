40aad3ae0747e56d9a8ca0d955a97a7aa49f0740
akmanning <amanning@broadinstitute.org>
2018-12-07 15:59:00 -0500

    do.saige.data input function

--- genomewide_summary/genomewide_summary_functions.R
+++ genomewide_summary/genomewide_summary_functions.R
@@ -1,2 +1,43 @@
 # Functions to create paper-ready tables & figures of genome-wide sequence association studies
 
+# variables that need to be defined:
+#  google.bucket.loc <- "gs:/.../"
+
+lapply(c("qqman","data.table","tools","RColorBrewer"), library, character.only = TRUE)
+
+## SAIGE Results
+do.saige.data <- function(saige.file) {
+    if(!file.exists(saige.file)) {
+        print(paste("Copying",saige.file,"from google bucket"))
+        system(paste("gsutil cp ",google.bucket.loc,saige.file," ",saige.file,sep=""))
+    }
+    assoc.data <- fread(paste("gunzip -c",saige.file),data.table=T,showProgress=T)
+    print(dim(assoc.data))
+    print(head(assoc.data))
+    print(tail(assoc.data))
+    
+    label <- saige.file
+
+    assoc.data$MarkerName <- paste(assoc.data$CHR,"-",assoc.data$POS,"-",assoc.data$Allele1,"-",assoc.data$Allele2,sep="")
+    
+    assoc.data$minor.allele <- ifelse(assoc.data$AF_Allele2<0.5,"alt","ref")
+    assoc.data$maf <- ifelse(assoc.data$AF_Allele2<0.5,assoc.data$AF_Allele2,1-assoc.data$AF_Allele2)
+    assoc.data$mac <- ifelse(assoc.data$AF_Allele2<0.5,assoc.data$AC_Allele2,2*assoc.data$N-assoc.data$AC_Allele2)
+
+    
+    setnames(assoc.data, c("CHR","POS","Allele1","Allele2","N","p.value","AF_Allele2"), 
+         c("chr","pos","ref","alt","n","pvalue","ALTFreq"))    
+    assoc.data <- assoc.data[,list(MarkerName,chr,pos,ref,alt,minor.allele,maf,mac,n,pvalue,SNPID,BETA,SE,ALTFreq)]
+
+    fwrite(assoc.data[pvalue<5e-6,],file=paste(label,"Plt5e-6.csv",sep="_"),quote=FALSE)
+    fwrite(assoc.data[pvalue<0.01,],file=paste(label,"Plt0.01.csv",sep="_"),quote=FALSE)
+    fwrite(assoc.data,file=paste(label,"allvariants.csv",sep="_"),quote=FALSE)
+
+    system(paste("gsutil cp ",label,"_Plt5e-6.csv ",google.bucket.loc,sep=""))
+    system(paste("gsutil cp ",label,"_Plt0.01.csv ",google.bucket.loc,sep=""))
+    system(paste("gzip -f ",label,"_allvariants.csv" ,sep=""))
+    system(paste("gsutil cp ",label,"_allvariants.csv.gz ",google.bucket.loc,sep=""))
+    save(assoc.data,file=paste(label,".RData",sep=""))
+    print(head(assoc.data))
+
+}
